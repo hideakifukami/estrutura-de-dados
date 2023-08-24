fffff24c7738be2e03dd83172bf0ad0d40deee55
Hideaki Fukami <107219109+hideakifukami@users.noreply.github.com>
2023-08-24 10:27:20 -0300

    att

---
+++ Aula01/src/exercicio01/Exercicio0.java
@@ -0,0 +1,19 @@
+package exercicio01;
+
+public class Exercicio0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;	
+	      
+        System.out.println("Números pares: ");   
+      
+        for (i = 1; i <= 10; i++) {
+          if (i % 2 == 0) {
+        	  System.out.println("\n" + i);  
+          }                 
+         
+        }
+	}
+
+}
---
+++ Aula01/src/exercicio01/Exercicio1.java
@@ -0,0 +1,18 @@
+package exercicio01;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;	
+	      
+        System.out.println("Números pares: ");   
+      
+        for (i = 2; i <= 10; i+=2) {
+          
+            System.out.println("\n" + i);        
+         
+        }
+	}
+
+}
