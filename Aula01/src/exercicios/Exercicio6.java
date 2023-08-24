56ff3806f0bf418646b2aaeda2fa7f7bae5f5ac7
Hideaki Fukami <107219109+hideakifukami@users.noreply.github.com>
2023-08-24 11:22:15 -0300

    att

---
+++ Aula01/src/exercicios/Exercicio6.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num, i, j;
+		
+		do {
+			System.out.println("Informe um número: ");
+			num = sc.nextInt();
+			
+			for(i = 1; i <= num; i++) {
+				System.out.println("");
+				for(j = 1; j <= i; j++) {
+					System.out.print(i);
+				}
+				
+			}
+			
+			System.out.println("\n");
+			
+		} while (num!=0);
+		
+		
+	}
+}
