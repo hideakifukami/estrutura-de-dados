bea89ed84403fe2b06cf6798e7d563f20fb5dc63
hideakifukami <denys.fukami@gmail.com>
2023-10-26 10:06:18 -0300

    bubblesort e bubblesortrec

--- InserctionSort/src/insertSort/Principal.java
+++ InserctionSort/src/insertSort/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 	private static int valores[] = {1,5,6,2,3,7,3,5,9};
 
 	public static void main(String[] args) {
-		inserctionSort(valores);
+		// inserctionSort(valores);
+		bubbleSortRec(valores);
 
 		for (int i = 0; i < valores.length; i++) {
 			System.out.print(valores[i]);
@@ -24,5 +25,37 @@ public class Principal {
 			valores[indiceMenor] = valores[i];
 			valores[i] = menor;
 		}
+	}
+	
+	private static void bubbleSort(int[] valores) {
+		int aux,j;
+		for (int i = 0; i < valores.length; i++) {			
+			for (j = 1; j < valores.length; j++) {
+				if (valores[j-1] > valores[j]) {
+					aux = valores[j];
+					valores[j] = valores[j-1];
+					valores[j-1] = aux;
+				}
+			}
+		}
+	}
+	
+	private static void bubbleSortRec(int[] valores) {
+		int aux,j;
+		boolean x = false;
+		for (int i = 1; i < valores.length; i++) {			
+			if (valores[i-1] > valores[i]) {
+				aux = valores[i];
+				valores[i] = valores[i-1];
+				valores[i-1] = aux;
+				x = true;
+			}			
+		}
+		
+		if (x) {
+			bubbleSortRec(valores);
+			x = false;
+		}
+	
 	}
 }
